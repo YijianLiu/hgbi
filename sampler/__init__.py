31898d850dc058130dbc8528de635894b8e3c3a2
liuyijian <1575797665@qq.com>
2023-05-22 23:25:21 +0800

    update dataset

---
+++ sampler/__init__.py
@@ -0,0 +1,3 @@
+from .hde_sampler import HDESampler
+from .HGT_sampler import HGTsampler
+from .HAN_Sampler import HANSampler
---
+++ test.py
@@ -0,0 +1,15 @@
+import g_library
+
+ds_node = g_library.construct_dataset(name = 'acm4NSHE',task = 'node_classification')
+'''
+ds_node contains dataset details, e.g. in_dim, meta_paths, category, num_classes
+ds_node.g is Graph on DGL format
+'''
+
+ds_link =  g_library.construct_dataset(name = 'HGBl-LastFM',task = 'link_prediction')
+print()
+'''
+ds_link contains dataset details, e.g. target_link, target_link_r, node_type,
+ds_link.g is Graph on DGL format
+'''
+
